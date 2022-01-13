98240c12985eabf9400c0da568247081fcc7b3d5
Popov-I-R <farm03007@gmail.com>
2022-01-13 10:28:00 +0200

    Create Sum Positive

---
+++ 1. Types and Variables/4.Sum Positive.js
@@ -0,0 +1,50 @@
+// Sum Positive
+
+// Write a program that reads a positive integer (n) You must print the sum of all integers from 1 to n.
+// Hint
+
+//     You could use the formula sum = n(n+1)/2*
+
+// Input
+
+//     On the first line you will receive a number (n)
+
+// Output
+
+//     You should print the sum of the integers from 1 to n
+
+// Input
+
+// 3
+
+// Output
+
+// 6
+
+// Input
+
+// 5
+
+// Output
+
+// 15
+
+// ------------------SOLUTION------------------
+
+// Input.
+let input = ['5'];
+
+// Custom implementation of read and print. Do not touch : )
+let print = this.print || console.log;
+let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
+
+// Solution
+
+let n = Number(gets());
+let sum = n * (n + 1) / 2;
+
+print(`${sum}`)
+
+
+
+
