4ef262d9cab63dd291412a082980f377d887c133
Popov-I-R <farm03007@gmail.com>
2022-01-15 01:10:17 +0200

    1. Exchange If Greater added

---
+++ 2. Conditional Statements/1. Exchange If Greater.js
@@ -0,0 +1,38 @@
+/* Write an if-statement that takes two double variables a and b and exchanges their values if the first one is greater than the second one. As a result print the values a and b, separated by a space.
+
+
+Examples:
+a       b       result
+5 	    2 	    2 5
+-------------------------
+3 	    4 	    3 4
+-------------------------
+5.5 	4.5 	4.5 5.5
+-------------------------
+*/ 
+
+//  ------------------SOLUTION------------------
+
+// Input.
+let input = [
+    '5.5',
+    '4.5',
+];
+
+// Custom implementation of read and print. Do not touch : )
+let print = this.print || console.log;
+let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
+
+// Solution
+
+let a = +gets();
+let b = +gets();
+
+let result;
+
+if (a > b)
+    result = console.log(`${b} ${a}`)
+else
+    result = console.log(`${a} ${b}`);
+
+return result;
