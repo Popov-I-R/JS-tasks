f5e576d70bf18a6fa3ba9f03c5ebaed28add9cdd
Popov-I-R <farm03007@gmail.com>
2022-02-08 15:04:27 +0200

    Update 7. Sort 3 Numbers with Nested Ifs.js

--- 2. Conditional Statements/7. Sort 3 Numbers with Nested Ifs.js
+++ 2. Conditional Statements/7. Sort 3 Numbers with Nested Ifs.js
@@ -3,16 +3,12 @@
 Write a program that enters 3 real numbers and prints them sorted in descending order. * Use nested if statements.
 
 Note: Don’t use arrays and the built-in sorting functionality.
- */
-
-
-// ------------------ SOLUTION---------------
-
-
+ 
 
 
 Examples:
 a           	b           	c           	result
+<<<<<<< Updated upstream
 5 	           1             2 	            5 2 1
 -2 	          -2	         1 	            1 -2 -2
 -2 	           4 	         3 	            4 3 -2
@@ -21,7 +17,17 @@ a           	b           	c           	result
 10 	           20         	30              	30 20 10
 1 	           1 	         1           	   1 1 1
 
+=======
+5           	1 	            2           	5   2   1
+-2             -2            	1           	1  -2  -2
+-2          	4           	3           	4   3  -2
+0              -2.5         	5           	5   0  -2.5
+-1.1           -0.5 	       -0.1 	       -0.1 -0.5 -1.1
+10 	            20 	            30          	30  20  10
+1           	1           	1           	1   1   1
+>>>>>>> Stashed changes
 
+ ------------------ SOLUTION--------------- */
 
 let input = ['5', '1', '2'];
 
