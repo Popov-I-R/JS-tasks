bed8afa236d65eac4035b800f69ffef187019c95
Popov-I-R <90955474+Popov-I-R@users.noreply.github.com>
2022-02-08 14:19:48 +0200

    Update 5.Interest.js

--- 1. Types and Variables/5.Interest.js
+++ 1. Types and Variables/5.Interest.js
@@ -1,34 +1,26 @@
-// Interest
 
 // You have deposited a sum into your bank account for 3 years. The bank has announced an interest of 5% per year. Each time the interest is calculated and added to your deposit. You have to calculate the amount in your deposit for each year.
 // Input
-
 //     On the only line you will receive a number (n) with the sum deposited
 
 // Output
-
 //     You should print the amount in your deposit for each of the 3 years
 
 // Constraints
-
 //     You must print the number with two numbers after the decimal point. The rules of math for rounding apply here
 
 // Input
-
 // 1000
 
 // Expected Output
-
 // 1050.00
 // 1102.50
 // 1157.63
 
 // Input
-
 // 2000
 
 // Expected Output
-
 // 2100.00
 //  2205.00
 // 2315.25
